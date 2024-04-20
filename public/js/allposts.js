d334e03870fbe999887ec96dfcd5f9be671b1f56
XuanzhaoS <t3zhxu00@students.oamk.fi>
2024-04-20 22:48:00 +0300

    Add eventlistener to like button to change color

--- public/js/allposts.js
+++ public/js/allposts.js
@@ -1,19 +1,19 @@
-// // get btn-like element
-// const likeButton = document.querySelector(".btn-like");
+// get btn-like element
+const likeButton = document.querySelector(".btn-like");
 
-// // addEventListener to btn-like
-// likeButton.addEventListener("click", function () {
-//   const currentFillColor = likeButton.style.fill;
+// addEventListener to btn-like
+likeButton.addEventListener("click", function () {
+  const currentFillColor = likeButton.style.fill;
 
-//   // Check the color condition
-//   if (currentFillColor === "red") {
-//     likeButton.style.fill = "";
-//     likeButton.style.stroke = "";
-//   } else {
-//     likeButton.style.fill = "red";
-//     likeButton.style.stroke = "red";
-//   }
-// });
+  // Check the color condition
+  if (currentFillColor === "red") {
+    likeButton.style.fill = "";
+    likeButton.style.stroke = "";
+  } else {
+    likeButton.style.fill = "red";
+    likeButton.style.stroke = "red";
+  }
+});
 
 
 
