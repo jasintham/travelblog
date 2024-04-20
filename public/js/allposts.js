d67d5c3baea60c6a4f6f053110dd56f1ecc67edc
XuanzhaoS <t3zhxu00@students.oamk.fi>
2024-04-20 22:42:49 +0300

    Connect to backend

--- public/js/allposts.js
+++ public/js/allposts.js
@@ -1,16 +1,41 @@
-// get btn-like element
-const likeButton = document.querySelector(".btn-like");
+// // get btn-like element
+// const likeButton = document.querySelector(".btn-like");
 
-// addEventListener to btn-like
-likeButton.addEventListener("click", function () {
-  const currentFillColor = likeButton.style.fill;
+// // addEventListener to btn-like
+// likeButton.addEventListener("click", function () {
+//   const currentFillColor = likeButton.style.fill;
 
-  // Check the color condition
-  if (currentFillColor === "red") {
-    likeButton.style.fill = "";
-    likeButton.style.stroke = "";
-  } else {
-    likeButton.style.fill = "red";
-    likeButton.style.stroke = "red";
-  }
-});
+//   // Check the color condition
+//   if (currentFillColor === "red") {
+//     likeButton.style.fill = "";
+//     likeButton.style.stroke = "";
+//   } else {
+//     likeButton.style.fill = "red";
+//     likeButton.style.stroke = "red";
+//   }
+// });
+
+
+
+fetch('http://localhost:3001/allposts/getAllPosts' ,{
+        method: 'GET',
+        headers : {
+            'Content-Type':'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+    })
+
+    .then((response) =>
+    {
+        return response.json();
+    })
+
+    .then((response)=>
+    {          
+        
+        response.forEach(element => {
+
+          console.log(element)
+          
+        });
+    })
