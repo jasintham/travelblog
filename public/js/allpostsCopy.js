78cbd163749a3ec682d0d20dd37f2fccf797a04b
XuanzhaoS <t3zhxu00@students.oamk.fi>
2024-04-22 19:37:12 +0300

    Add icons for delete and edit

--- public/js/allpostsCopy.js
+++ public/js/allpostsCopy.js
@@ -69,10 +69,10 @@ function createPostElement(post) {
         d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
       />
     </svg>
-    <p class="like-number">${post.comments_count}</p>
+    <p class="like-number">${post.likes_count}</p>
   </div>
   <div class="comment">
-    <!-- Comment icon with link to specific post page -->
+    <!-- comment icon with link to specific post page -->
     <a href="./post.html">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -90,6 +90,19 @@ function createPostElement(post) {
       </svg>
     </a>
     <p class="comment-number">${post.comments_count}</p>
+  </div>
+  <div class="edit">
+    <!-- Edit icon with link to specific post page -->
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="btn-edit">
+    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+  </svg>
+  </div>
+  <div class="delete">
+    <!-- Delete icon with link to specific post page -->
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="btn-delete">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+
   </div>
 </div>
 </div>
